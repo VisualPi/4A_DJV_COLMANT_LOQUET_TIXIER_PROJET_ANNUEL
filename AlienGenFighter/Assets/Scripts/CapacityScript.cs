046b2cb83c3bca60bc47fc538ad1c8d91d8c3c77
VisualPi <gintakun2@yahoo.fr>
2015-04-03 00:31:59 +0200

    Ajout de EntityScript + CapacityScript et du Entity.prefab

---
+++ AlienGenFighter/Assets/Scripts/CapacityScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public enum ECapacity
+{
+	Walk = 0, Run, Jump, Swim, Fly
+};
+
+
+public class CapacityScript : MonoBehaviour
+{
+	private const byte NbCapacity = 5;
+	private Dictionary<ECapacity, bool> _capacities;
+
+	// Use this for initialization
+	void Start()
+	{
+		for (ECapacity i = 0; i < (ECapacity)NbCapacity; ++i)
+			_capacities.Add(i, false);
+
+		//foreach(ECapacity cap in ECapacity.GetValues(typeof(ECapacity)))
+		//	_capacities.Add(cap, false);
+	}
+
+	public bool GetCapacityAt(ECapacity cap)
+	{
+		return _capacities[cap];
+	}
+
+	public void SetCapacityAt(ECapacity cap, bool value)
+	{
+		_capacities[cap] = value;
+	}
+}
---
+++ AlienGenFighter/Assets/Scripts/EntityScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EntityScript : MonoBehaviour
+{
+
+	private DnaScript		_dna;
+	private CapacityScript	_capacities;
+
+	public Transform _transform;
+	public GameObject _gameObject;
+	public Rigidbody _rigidbody;
+
+
+	//public EntityScript(DnaScript dna) //inutile ?!
+	//{
+	//	_dna = dna;
+	//
+	//}
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public DnaScript GetDNA()
+	{
+		return _dna;
+	}
+	public void SetDNA(DnaScript dna)
+	{
+		_dna = dna;
+	}
+	public CapacityScript GetCapacity()
+	{
+		return _capacities;
+	}
+	public void SetCapacity(CapacityScript cap)
+	{
+		_capacities = cap;
+	}
+}
